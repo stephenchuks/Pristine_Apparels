5f95825143a7c2c1f7be160dd1d3ec3129285961
stephenchuks <104122101+stephenchuks@users.noreply.github.com>
2023-04-17 16:41:10 +0100

    Created Login Functinality

--- client_side/src/App.vue
+++ client_side/src/App.vue
@@ -61,6 +61,8 @@
   </div>
 </template>
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
@@ -72,6 +74,14 @@ export default {
   },
   beforeCreate() {
     this.$store.commit('initializeStore')
+
+    const token = this.$store.state.token
+
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = "Token" + token
+    } else {
+      axios.defaults.headers.common['Authorization'] = ''
+    }
   },
   mounted() {
     this.cart = this.$store.state.cart
