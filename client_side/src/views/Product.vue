bd82d78bfa1974c07b9217aa2cba3b347cc36683
stephenchuks <104122101+stephenchuks@users.noreply.github.com>
2023-06-03 09:51:43 +0100

    hot fix

--- client_side/src/views/Product.vue
+++ client_side/src/views/Product.vue
@@ -54,7 +54,7 @@ export default {
         .get(`/api/v1/products/${category_slug}/${product_slug}`)
         .then(response => {
             this.product = response.data
-            document.title = this.product.name + ' | Pristine Apparels'
+            document.title = this.product.name + ' | PristineApparels'
         })
         .catch(error => {
             console.log(error)
